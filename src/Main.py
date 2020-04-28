2015912b40313ac660e30ed8d019b955797e5fe8
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-04-28 14:35:06 -0400

    Created window

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,36 @@
+# Library imports
+import pygame
+
+# Local imports
+# (None)
+
 # Logger import
 from Logger.Logger import logger
 
+# Initialize pygame
+pygame.init()
 
+# Screen size constants
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+# Set up the drawing window
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 if __name__ == "__main__":
     logger.debug("Start")
+
+    running = True
+
+    # Main Loop
+    while running:
+
+        # Loop through the event queue
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Flip the display
+        pygame.display.flip()
+
+    pygame.quit()
